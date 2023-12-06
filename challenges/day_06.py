dd3c9376042fc29517e60b2533e4651664fb735f
Marcelo Tyszler <marcelo@blonkconsultants.nl>
2023-12-06 09:50:37 +0100

    Day 6, easier than day 5

---
+++ challenges/day_06.py
@@ -0,0 +1,15 @@
+from functions_day_06 import *
+
+# challenge 1
+print("challenge 1")
+race_1 = cross_points(38, 241)
+race_2 = cross_points(94, 1549)
+race_3 = cross_points(79, 1074)
+race_4 = cross_points(70, 1091)
+print(race_1*race_2*race_3*race_4)
+
+# challenge 2
+print("challenge 2")
+race_1 = cross_points(38947970, 241154910741091)
+print(race_1)
+
