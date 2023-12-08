6bffecb6d73c6bc6b697e996debe3c6b8c5d9a41
Marcelo Tyszler <marcelo@blonkconsultants.nl>
2023-12-08 13:18:16 +0100

    Clean up day 5

--- src/functions_day_05.py
+++ src/functions_day_05.py
@@ -1,89 +1,3 @@
-def parse_input_file(input_file: str) -> float:
-    """
-
-    Args:
-        input_file:
-
-    Returns:
-
-    """
-    input_file = open(input_file, 'r')
-    lines = input_file.readlines()
-
-    seeds = []
-    seed_to_soil = {}
-    soil_to_fertilizer = {}
-    fertilizer_to_water = {}
-    water_to_light = {}
-    light_to_temperature = {}
-    temperature_to_humidity = {}
-    humidity_to_location = {}
-
-    target_dict = {}
-    max_counter = 0
-
-    for this_line in lines:
-        if 'seeds' in this_line:
-            print("seeds")
-            _, seed_str = this_line.split(":")
-            seeds = [int(x) for x in seed_str.strip().split(" ")]
-            max_counter = max(seeds)
-
-        elif "seed-to-soil" in this_line:
-            print("seed-to-soil")
-            target_dict = seed_to_soil
-        elif "soil-to-fertilizer" in this_line:
-            print("soil-to-fertilizer")
-            target_dict = soil_to_fertilizer
-        elif "fertilizer-to-water" in this_line:
-            target_dict = fertilizer_to_water
-        elif "water-to-light" in this_line:
-            target_dict = water_to_light
-        elif "light-to-temperature" in this_line:
-            target_dict = light_to_temperature
-        elif "temperature-to-humidity" in this_line:
-            target_dict = temperature_to_humidity
-        elif "humidity-to-location" in this_line:
-            target_dict = humidity_to_location
-
-        elif this_line.strip() == "":
-            continue
-
-        else:
-            dest_range_start, source_range_start, range_len = this_line.strip().split(" ")
-
-            for i in range(int(range_len)):
-                target_dict[int(source_range_start) + i] = int(dest_range_start) + i
-                if int(dest_range_start) + i > max_counter:
-                    max_counter = int(dest_range_start) + i
-
-    # complete dicts:
-
-    for this_dict in [seed_to_soil,
-                      soil_to_fertilizer,
-                      fertilizer_to_water,
-                      water_to_light,
-                      light_to_temperature,
-                      temperature_to_humidity,
-                      humidity_to_location]:
-        print("complete dict")
-        for i in range(max_counter + 1):
-            if i not in this_dict.keys():
-                this_dict[i] = i
-
-    # find the lowest location:
-    location = 10000000
-    this_location = 10000000
-    for this_seed in seeds:
-
-        this_location = humidity_to_location[temperature_to_humidity[
-            light_to_temperature[water_to_light[fertilizer_to_water[soil_to_fertilizer[seed_to_soil[this_seed]]]]]]]
-        if this_location < location:
-            location = this_location
-
-    return this_location
-
-
 def parse_input_file_v2(input_file: str) -> float:
     """
 
@@ -160,7 +74,7 @@ def find_key(key: int, mapping: dict) -> int:
     found_key = -999
     for this_key in mapping.keys():
 
-        if this_key[0] <= key <= this_key[1]:
+        if this_key[0] <= key < this_key[1]:
             found_key = mapping[this_key][0] + (key - this_key[0])
             break
 
@@ -182,7 +96,7 @@ def parse_input_file_v3(input_file: str) -> float:
     input_file = open(input_file, 'r')
     lines = input_file.readlines()
 
-    seeds = {}
+    seeds = []
     seed_to_soil = {}
     soil_to_fertilizer = {}
     fertilizer_to_water = {}
@@ -198,7 +112,7 @@ def parse_input_file_v3(input_file: str) -> float:
             _, seed_str = this_line.split(":")
             seeds_base = [int(x) for x in seed_str.strip().split(" ")]
             for i in range(0, len(seeds_base), 2):
-                seeds[seeds_base[i]] = seeds_base[i + 1]
+                seeds.append((seeds_base[i], seeds_base[i] + seeds_base[i + 1]))
 
         elif "seed-to-soil" in this_line:
             target_dict = seed_to_soil
@@ -225,21 +139,21 @@ def parse_input_file_v3(input_file: str) -> float:
                 (int(dest_range_start), int(dest_range_start) + int(range_len))
 
     # find the lowest location:
-    location = 1000000000000000000
-    for this_seed_start in seeds.keys():
-        print('--------------------------')
-        next_key = map(lambda x: find_location(key=x, list_of_mappers=[seed_to_soil,
-                                                                       soil_to_fertilizer,
-                                                                       fertilizer_to_water,
-                                                                       water_to_light,
-                                                                       light_to_temperature,
-                                                                       temperature_to_humidity,
-                                                                       humidity_to_location]),
-                       range(this_seed_start, this_seed_start + seeds[this_seed_start]))
-        next_key = min(list(next_key))
-        if next_key < location:
-            print(next_key)
-            location = next_key
+    location_ranges = []
+    for this_seed_range in seeds:
+        next_range = [this_seed_range]
+        for search_mapper in [seed_to_soil,
+                              soil_to_fertilizer,
+                              fertilizer_to_water,
+                              water_to_light,
+                              light_to_temperature,
+                              temperature_to_humidity,
+                              humidity_to_location]:
+            next_range = find_ranges(next_range, search_mapper)
+        location_ranges.extend(next_range)
+
+    # find the lowest location:
+    location = min([x[0] for x in location_ranges])
 
     return location
 
@@ -250,3 +164,45 @@ def find_location(key: int, list_of_mappers: list[dict]) -> int:
         next_key = find_key(next_key, search_mapper)
 
     return next_key
+
+
+def find_ranges(key: (int, int), mapping: dict) -> list[(int, int)]:
+    translated_ranges = []
+    still_unprocessed_ranges = key.copy()
+
+    for this_key in mapping.keys():
+        unprocessed_ranges = list(still_unprocessed_ranges.copy())
+        still_unprocessed_ranges = set()
+
+        while len(unprocessed_ranges) > 0:
+            this_unprocessed_range = unprocessed_ranges.pop()
+            lower = this_unprocessed_range[0]
+            upper = this_unprocessed_range[1]
+
+            # case 1 range does not overlap mapping range:
+            if lower >= this_key[1] or upper < this_key[0]:
+                still_unprocessed_ranges.add((lower, upper))
+                continue
+
+            # lower number
+            if this_key[0] <= lower < this_key[1]:
+                # lower within range:
+                mapped_lower = mapping[this_key][0] + (lower - this_key[0])
+            else:
+                # lower not within range, split:
+                still_unprocessed_ranges.add((lower, this_key[0]))
+                mapped_lower = mapping[this_key][0]
+
+            #  higher number:
+            if this_key[0] <= upper < this_key[1]:
+                # upper within range:
+                mapped_upper = mapping[this_key][0] + (upper - this_key[0])
+                translated_ranges.append((mapped_lower, mapped_upper))
+            else:
+                # upper not within range, split:
+                translated_ranges.append((mapped_lower, mapping[this_key][1]))
+                still_unprocessed_ranges.add((this_key[1], upper))
+
+    translated_ranges.extend(list(still_unprocessed_ranges))
+
+    return translated_ranges
--- test/test_day_05.py
+++ test/test_day_05.py
@@ -3,11 +3,6 @@ import pytest
 from functions_day_05 import *
 
 
-def test_parse_input():
-    lowest_location = parse_input_file('../input_files/input_day_05_example.txt')
-    assert (lowest_location == 35)
-
-
 def test_parse_input_v2():
     lowest_location = parse_input_file_v2('../input_files/input_day_05_example.txt')
     assert (lowest_location == 35)
@@ -16,3 +11,8 @@ def test_parse_input_v2():
 def test_parse_input_v3():
     lowest_location = parse_input_file_v3('../input_files/input_day_05_example.txt')
     assert (lowest_location == 46)
+
+
+def test_find_ranges():
+    new_ranges = find_ranges((79,79+14), {(98, 100): (50, 52), (50, 98): (52, 100)})
+    assert (new_ranges == [(81,81+14)])
