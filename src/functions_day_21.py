794d4466db35baf873b2596950ebbdeb0d69f9ce
mtyszler <tyszler.jobs@gmail.com>
2023-12-26 19:46:33 +0100

    Revert "temp non-functioning solution"
    This reverts commit 0344451573e353f2853843734d36df3a57a770d0.

--- src/functions_day_21.py
+++ src/functions_day_21.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def parse_input(input_file: str) -> [np.ndarray, list[int, int]]:
+def parse_input(input_file: str) -> [np.ndarray, (int, int)]:
     input_file = open(input_file, 'r')
     lines = input_file.readlines()
 
@@ -15,54 +15,40 @@ def parse_input(input_file: str) -> [np.ndarray, list[int, int]]:
         grid_raw.append([x for x in this_line.strip()])
         r += 1
 
-    return np.array(grid_raw), [s_row, s_col]
+    return np.array(grid_raw), (s_row, s_col)
 
 
-def find_possibilities(grid: np.ndarray, start_location: list[int, int], n_steps: int) -> int:
+def find_possibilities(grid: np.ndarray, start_location: (int, int), n_steps: int) -> int:
     # assuming even steps
     if n_steps % 2 != 0:
         raise Exception('function works with even n_steps')
 
-    to_check = [[start_location, 0, [0, 0]]]
-    garden_plots = set()
-    garden_plots.add((start_location[0], start_location[1], 0, 0))
+    to_check = [[start_location, 0]]
+    garden_plots = set([start_location])
 
     while len(to_check) > 0:
 
-        this_location, this_steps, this_realm = to_check.pop(0)
+        this_location, this_steps = to_check.pop(0)
         if this_steps == n_steps:
             continue
 
         this_r, this_c = this_location
-        candidates = [[this_r, this_c + 1],
-                      [this_r, this_c - 1],
-                      [this_r - 1, this_c],
-                      [this_r + 1, this_c]]
+        candidates = [(this_r, this_c + 1),
+                      (this_r, this_c - 1),
+                      (this_r - 1, this_c),
+                      (this_r + 1, this_c)]
 
         for this_candidate in candidates:
-            new_realm = this_realm.copy()
-            if this_candidate[0] < 0:
-                this_candidate[0] = grid.shape[0] + this_candidate[0]
-                new_realm[0] -= 1
+            if this_candidate[0] < 0 or this_candidate[1] < 0 or \
+                    this_candidate[0] >= grid.shape[0] or this_candidate[1] >= grid.shape[1]:
+                continue
 
-            if this_candidate[1] < 0:
-                this_candidate[1] = grid.shape[1] + this_candidate[1]
-                new_realm[1] -= 1
-
-            if this_candidate[0] >= grid.shape[0]:
-                this_candidate[0] = this_candidate[0] - grid.shape[0]
-                new_realm[0] += 1
-
-            if this_candidate[1] >= grid.shape[1]:
-                this_candidate[1] = this_candidate[1] - grid.shape[1]
-                new_realm[1] += 1
-
-            if grid[*this_candidate] == '.' or grid[*this_candidate] == 'S':
+            if grid[this_candidate] == '.' or grid[this_candidate] == 'S':
                 if (this_steps + 1) % 2 == 0:
-                    if (this_candidate[0], this_candidate[1], this_realm[0], new_realm[1]) not in garden_plots:
-                        garden_plots.add((this_candidate[0], this_candidate[1], new_realm[0], new_realm[1]))
-                        to_check.append([this_candidate, this_steps + 1, new_realm])
+                    if this_candidate not in garden_plots:
+                        garden_plots.add(this_candidate)
+                        to_check.append([this_candidate, this_steps + 1])
                 else:
-                    to_check.append([this_candidate, this_steps + 1, new_realm])
+                    to_check.append([this_candidate, this_steps + 1])
 
     return len(garden_plots)
--- test/test_day_21.py
+++ test/test_day_21.py
@@ -6,7 +6,7 @@ from functions_day_21 import *
 def test_parse():
     grid, start_location = parse_input('../input_files/input_day_21_example.txt')
 
-    assert (start_location == [5, 5])
+    assert (start_location == (5, 5))
 
 
 def test_steps():
@@ -14,30 +14,3 @@ def test_steps():
     n_garden = find_possibilities(grid, start_location, n_steps=6)
 
     assert (n_garden == 16)
-
-def test_steps_v3():
-    grid, start_location = parse_input('../input_files/input_day_21_example.txt')
-    n_garden = find_possibilities(grid, start_location, n_steps=10)
-
-    assert (n_garden == 50)
-
-def test_steps_v4():
-    grid, start_location = parse_input('../input_files/input_day_21_example.txt')
-    n_garden = find_possibilities(grid, start_location, n_steps=50)
-
-    assert (n_garden == 1594)
-
-@pytest.mark.parametrize("steps,  response",
-                         [(6, 16),
-                          (10, 50),
-                          (50, 1594),
-                          (100, 6536),
-                          (500, 167004),
-                          (1000, 558697),
-                          (5000, 16733044),
-                          ])
-def test_steps_v2(steps, response):
-    grid, start_location = parse_input('../input_files/input_day_21_example.txt')
-    n_garden = find_possibilities(grid, start_location, n_steps=steps)
-
-    assert (n_garden == response)
